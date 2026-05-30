1045078f2c839d624c2595507b9bb90a71a02405
Rcwalter <rcwalter24@outlook.com>
2026-05-31 00:25:26 +0800

    fix(ci): switch to full vite-plugin-electron to avoid path normalization issues

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import electron from 'vite-plugin-electron/simple'
+import electron from 'vite-plugin-electron'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
@@ -9,14 +9,17 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 export default defineConfig({
   plugins: [
     react(),
-    electron({
-      main: {
+    electron([
+      {
         entry: path.resolve(__dirname, 'electron/main.js'),
       },
-      preload: {
-        input: path.resolve(__dirname, 'electron/preload.js'),
+      {
+        entry: path.resolve(__dirname, 'electron/preload.js'),
+        onstart(options) {
+          options.reload()
+        },
       },
-    }),
+    ]),
   ],
 })
 
